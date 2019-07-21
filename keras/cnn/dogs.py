ab6bfcaf022d331010ea353a4f59c82c599a4523
Max <max.mjm.melo@gmail.com>
2019-07-21 15:51:56 -0400

    mudanças no save/load e arquitetura da CNN

--- keras/cnn/dogs.py
+++ keras/cnn/dogs.py
@@ -8,6 +8,9 @@ from IPython.display import display
 from PIL import Image
 from keras.preprocessing import image
 import numpy as np
+import os
+#from keras.models import model_from_json
+from keras.models import load_model
 
 
 class ClassifyDogsAndCats:
@@ -15,13 +18,22 @@ class ClassifyDogsAndCats:
         self.classifier = Sequential()
 
     def train(self):
-        #conv
+        #conv 1
         self.classifier.add( Convolution2D(32, 3,3, input_shape = (64,64,3), activation = 'relu') )
 
-        #pooling
+        #pooling 1
         self.classifier.add(MaxPooling2D(pool_size = (2,2)))
 
-        #flatten
+        #flatten 1
+        #self.classifier.add(Flatten())
+
+        #conv 2
+        self.classifier.add( Convolution2D(32, 3,3, input_shape = (64,64,3), activation = 'relu') )
+
+        #pooling 2
+        self.classifier.add(MaxPooling2D(pool_size = (2,2)))
+
+        #flatten 2
         self.classifier.add(Flatten())
 
         #fc
@@ -61,36 +73,50 @@ class ClassifyDogsAndCats:
             steps_per_epoch = 8000,
             epochs = 10,
             validation_data = self.testSet,
-            validation_steps = 800
+            validation_steps = 800,
+            use_multiprocessing=True,
+            workers=4
         )
 
         # serialize model to JSON
-        model_json = self.classifier.to_json()
-        with open("model.json", "w") as json_file:
-            json_file.write(model_json)
+        #model_json = self.classifier.to_json()
+        #with open("model.json", "w") as json_file:
+        #    json_file.write(model_json)
         # serialize weights to HDF5
-        self.classifier.save_weights("model.h5")
+        #self.classifier.save_weights("model.h5")
+        
+        self.classifier.save("test_model.h5")
+        
         print("Saved model to disk")
 
     def loadClassifier(self):
         # load json and create model
-        json_file = open('model.json', 'r')
-        loaded_model_json = json_file.read()
-        json_file.close()
-        loaded_model = model_from_json(loaded_model_json)
+        #json_file = open('model.json', 'r')
+        #loaded_model_json = json_file.read()
+        #json_file.close()
+        #loaded_model = model_from_json(loaded_model_json)
         # load weights into new model
-        loaded_model.load_weights("model.h5")
+        #loaded_model.load_weights("model.h5")
+
+        #self.classifier = loaded_model
+
+        self.classifier = load_model("test_model.h5")
+
         print("Loaded model from disk")
 
         #compiling
-        self.classifier.compile( optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'] )
+        #self.classifier.compile( optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'] )
     
-    def test(self):
-        testImage = image.load_img("random1.png", target_size = (64,64))
+    def test(self, imageName):
+        
+        testImage = image.load_img(imageName, target_size = (64,64))
         testImage = image.img_to_array(testImage)
         testImage = np.expand_dims(testImage, axis = 0)
 
         result = self.classifier.predict(testImage)
+
+        print("details")
+        print(result)
 
         if result[0][0] > 0.5:
             return "dog"
@@ -121,7 +147,8 @@ def main():
         obj.loadClassifier()
     
     if menu > 0:
-        print( obj.test() ) 
+        for i in range(1,18):
+            print( obj.test("tests/test"+str(i)+".jpeg" ) ) 
 
 
 
